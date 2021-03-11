76736ae6f6ecbad8ebdca33428ec2faa5d60a1b1
ivargj <ivar.gj@outlook.com>
2021-03-11 23:15:37 +0100

    Now what

---
+++ src/test/kotlin/ivaralek/bilboka/BilbokaApplicationTests.kt
@@ -0,0 +1,13 @@
+package ivaralek.bilboka
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BilbokaApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
