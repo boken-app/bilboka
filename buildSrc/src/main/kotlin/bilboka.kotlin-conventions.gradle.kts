c59aa88f8712611fa47261cc2709d623941b9e02
ivargj <ivar.gj@outlook.com>
2021-05-27 01:30:07 +0200

    Potensiell workaround

--- buildSrc/src/main/kotlin/bilboka.kotlin-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/bilboka.kotlin-conventions.gradle.kts
@@ -9,3 +9,23 @@ repositories {
     mavenCentral()
 }
 
+// FIX KAPT incapacity to find the right variant
+// https://youtrack.jetbrains.com/issue/KT-31641
+val usage = Attribute.of("org.gradle.usage", Usage::class.java)
+
+dependencies {
+    attributesSchema {
+        attribute(usage)
+    }
+}
+
+configurations.all {
+    afterEvaluate {
+        if (isCanBeResolved && !attributes.contains(usage)) {
+            attributes {
+                attribute(usage, project.objects.named(Usage::class.java, "java-runtime"))
+            }
+        }
+    }
+}
+// END FIX KAPT incapacity to find the right variant
