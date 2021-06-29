74de5b9c7e931a7b2139488c928ecadca7e30cbe
ivargj <ivar.gj@outlook.com>
2021-06-30 00:55:14 +0200

    Fjerner redundante ting i build.gradle.

--- bilboka-messenger-integration/build.gradle.kts
+++ bilboka-messenger-integration/build.gradle.kts
@@ -12,28 +12,14 @@ repositories {
     mavenCentral()
 }
 
-dependencyManagement {
-    imports { mavenBom("org.springframework.boot:spring-boot-dependencies:2.4.4") }
-}
-
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
-    implementation("org.flywaydb:flyway-core")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("junit:junit:4.12")
     runtimeOnly("com.h2database:h2")
 
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-        freeCompilerArgs = listOf("-Xjsr305=strict")
-        jvmTarget = "11"
-    }
 }
 
 tasks.getByName<BootJar>("bootJar") {
