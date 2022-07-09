557e4ecad76b6619ec99363814e1743d1a90974d
IvarAlek <ivar.gj@hotmail.com>
2022-07-09 20:00:00 +0200

    Sender ID og logging

--- bilboka-messenger-integration/src/main/kotlin/bilboka/messenger/service/MessageResponderService.kt
+++ bilboka-messenger-integration/src/main/kotlin/bilboka/messenger/service/MessageResponderService.kt
@@ -3,6 +3,7 @@ package bilboka.messenger.service
 import bilboka.messenger.consumer.MessengerWebhookConsumer
 import bilboka.messenger.dto.FacebookEntry
 import bilboka.messenger.dto.FacebookMessage
+import org.json.JSONObject
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
@@ -25,23 +26,23 @@ class MessageResponderService {
             val senderPSID = messageEvent.sender["id"]
             logger.info(format("Sender PSID: %s", senderPSID))
 
-            logger.info("messageEvent.message=${messageEvent.message}")
+            logger.info("messageEvent.message=${JSONObject(messageEvent.message)}")
 
             val text = messageEvent.message?.message?.get("text")
             logger.info(format("Mottok melding: %s", text))
 
             // TODO behandle melding
 
-            sendReply(format("Du sendte melding: %s", text))
+            sendReply(format("Du sendte melding: %s", text), senderPSID!!)
         } else {
             logger.warn("Ugyldig lengde på Messaging. Forventet 1, var {}", entry.messaging.size)
         }
     }
 
-    private fun sendReply(text: String) {
+    private fun sendReply(text: String, recipientPSID: String) {
         messengerConsumer.sendMessage(
             FacebookMessage(
-                recipient = mapOf(),
+                recipient = mapOf(Pair("id", recipientPSID)),
                 message = mapOf(Pair("text", text))
             )
         )
