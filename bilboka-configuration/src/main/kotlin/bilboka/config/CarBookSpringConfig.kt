4982d440fabaea504d86d0e15cd9bb3b7cdce190
IvarAlek <ivar.gj@outlook.com>
2022-07-19 13:19:41 +0200

    Localeresolver

--- bilboka-configuration/src/main/kotlin/bilboka/config/CarBookSpringConfig.kt
+++ bilboka-configuration/src/main/kotlin/bilboka/config/CarBookSpringConfig.kt
@@ -4,6 +4,10 @@ import bilboka.core.repository.InMemoryStorage
 import bilboka.core.repository.VehicleRepository
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.LocaleResolver
+import org.springframework.web.servlet.i18n.SessionLocaleResolver
+import java.util.*
+
 
 @Configuration
 class CarBookSpringConfig {
@@ -11,6 +15,13 @@ class CarBookSpringConfig {
     @Bean
     fun vehicleRepository(): VehicleRepository {
         return InMemoryStorage()
+    }
+
+    @Bean
+    fun localeResolver(): LocaleResolver? {
+        val slr = SessionLocaleResolver()
+        slr.setDefaultLocale(Locale.GERMANY)
+        return slr
     }
 
 }
