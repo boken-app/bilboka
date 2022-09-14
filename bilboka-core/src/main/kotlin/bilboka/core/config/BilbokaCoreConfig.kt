7e4f4896ded901713f16358ee76be89f8a7e924d
IvarAlek <ivar.gj@hotmail.com>
2022-09-14 20:57:09 +0200

    Logge datasource creation

--- bilboka-core/src/main/kotlin/bilboka/core/config/BilbokaCoreConfig.kt
+++ bilboka-core/src/main/kotlin/bilboka/core/config/BilbokaCoreConfig.kt
@@ -3,6 +3,7 @@ package bilboka.core.config
 import bilboka.core.repository.VehicleRepository
 import com.zaxxer.hikari.HikariConfig
 import com.zaxxer.hikari.HikariDataSource
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
@@ -17,6 +18,7 @@ import javax.sql.DataSource
 @EnableJpaRepositories(basePackageClasses = [VehicleRepository::class])
 @EntityScan(basePackages = ["bilboka.core.domain"])
 class BilbokaCoreConfig {
+    private val logger = LoggerFactory.getLogger(javaClass)
 
     @Value("\${spring.datasource.url:null}")
     private val dbUrl: String? = null
@@ -24,6 +26,7 @@ class BilbokaCoreConfig {
     @Bean
     @ConditionalOnProperty("spring.datasource.url")
     fun dataSource(): DataSource? {
+        logger.debug("Creating datasource config for database $dbUrl")
         val config = HikariConfig()
         config.jdbcUrl = dbUrl
         return HikariDataSource(config)
