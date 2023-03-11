633ad434d48cac42cada1da56b6cfe72dff441e1
IvarAlek <ivar.gj@outlook.com>
2023-03-11 17:18:26 +0100

    Hjelp-option i persistent menu

--- bilboka-messenger-integration/src/main/kotlin/bilboka/messenger/ProfileConfig.kt
+++ bilboka-messenger-integration/src/main/kotlin/bilboka/messenger/ProfileConfig.kt
@@ -2,6 +2,8 @@ package bilboka.messenger
 
 import bilboka.messenger.consumer.MessengerProfileAPIConsumer
 import bilboka.messenger.dto.MessengerProfileRequest
+import bilboka.messenger.dto.PersistentMenu
+import bilboka.messenger.dto.PersistentMenuItem
 import org.slf4j.LoggerFactory
 import org.springframework.context.event.ContextStartedEvent
 import org.springframework.context.event.EventListener
@@ -19,7 +21,14 @@ class ProfileConfig(
         profileAPIConsumer.doProfileUpdate(
             MessengerProfileRequest(
                 listOf(
-
+                    PersistentMenu(
+                        callToActions = listOf(
+                            PersistentMenuItem(
+                                title = "Hjelp",
+                                payload = "hlp"
+                            )
+                        )
+                    )
                 )
             )
         )
