e6f2d02b39ff314a700ea1366014ed757e7e58f4
IvarAlek <ivar.gj@outlook.com>
2023-03-11 17:45:48 +0100

    Setter profilinfo som scheduled job istedenfor å bruke event listener.

--- bilboka-messenger-integration/src/main/kotlin/bilboka/messenger/ProfileConfig.kt
+++ bilboka-messenger-integration/src/main/kotlin/bilboka/messenger/ProfileConfig.kt
@@ -5,9 +5,10 @@ import bilboka.messenger.dto.MessengerProfileRequest
 import bilboka.messenger.dto.PersistentMenu
 import bilboka.messenger.dto.PersistentMenuItem
 import org.slf4j.LoggerFactory
-import org.springframework.context.event.ContextStartedEvent
-import org.springframework.context.event.EventListener
+import org.springframework.context.event.ContextRefreshedEvent
+import org.springframework.scheduling.annotation.Scheduled
 import org.springframework.stereotype.Component
+import java.util.concurrent.TimeUnit
 
 @Component
 class ProfileConfig(
@@ -15,22 +16,29 @@ class ProfileConfig(
 ) {
     private val logger = LoggerFactory.getLogger(javaClass)
 
-    @EventListener(ContextStartedEvent::class)
-    fun setProfileConfig(event: ContextStartedEvent) {
-        logger.info("Setting profile config")
-        profileAPIConsumer.doProfileUpdate(
-            MessengerProfileRequest(
-                listOf(
-                    PersistentMenu(
-                        callToActions = listOf(
-                            PersistentMenuItem(
-                                title = "Hjelp",
-                                payload = "hlp"
+    private var hasDone = false
+
+    @Scheduled(initialDelay = 10, timeUnit = TimeUnit.SECONDS)
+    fun setProfileConfig(event: ContextRefreshedEvent) {
+        if (!hasDone) {
+            logger.info("Setting profile config")
+            profileAPIConsumer.doProfileUpdate(
+                MessengerProfileRequest(
+                    listOf(
+                        PersistentMenu(
+                            callToActions = listOf(
+                                PersistentMenuItem(
+                                    title = "Hjelp",
+                                    payload = "hlp"
+                                )
                             )
                         )
                     )
                 )
             )
-        )
+            hasDone = true
+        } else {
+            logger.info("Profile config already set")
+        }
     }
 }
