bf83f0e3c1aebb3e2c2f2d9a37461bcbd77d2da5
IvarAlek <ivar.gj@hotmail.com>
2023-03-15 23:15:04 +0100

    DTO for vedlegg

--- bilboka-messenger-integration/src/main/kotlin/bilboka/messenger/dto/MessengerSendMessageRequest.kt
+++ bilboka-messenger-integration/src/main/kotlin/bilboka/messenger/dto/MessengerSendMessageRequest.kt
@@ -1,6 +1,7 @@
 package bilboka.messenger.dto
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.annotation.JsonValue
 import java.io.Serializable
 
 data class MessengerWebhookRequest(
@@ -32,8 +33,24 @@ data class FacebookMessage(
     val mid: String? = null,
     @Deprecated("Fjern denne, aner ikke hvorfor den nekter å kjøre uten")
     val seq: Long? = null,
-    val text: String?
+    val text: String? = null,
+    val attachment: Attachment? = null
 ) : Serializable
+
+data class Attachment(
+    val type: AttachmentType,
+    val payload: Any?
+)
+
+enum class AttachmentType(
+    @JsonValue val strVal: String
+) {
+    AUDIO("audio"),
+    FILE("file"),
+    IMAGE("image"),
+    TEMPLATE("template"),
+    VIDEO("video")
+}
 
 data class FacebookPostback(
     val mid: String? = null,
