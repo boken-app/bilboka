b0f57b92096c5f82202a912c8860cc0132de8599
IvarAlek <ivar.gj@outlook.com>
2023-03-20 15:05:52 +0100

    Versjons-oppdateringer! (Og key som er keyword og må i quotes)

--- bilboka-messenger-integration/src/main/kotlin/bilboka/messenger/resource/WebhookSignatureVerificationFilter.kt
+++ bilboka-messenger-integration/src/main/kotlin/bilboka/messenger/resource/WebhookSignatureVerificationFilter.kt
@@ -1,6 +1,9 @@
 package bilboka.messenger.resource
 
 import bilboka.messenger.MessengerProperties
+import jakarta.servlet.FilterChain
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.apache.commons.codec.binary.Hex
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpMethod
@@ -12,9 +15,6 @@ import org.springframework.web.filter.OncePerRequestFilter
 import java.security.InvalidKeyException
 import javax.crypto.Mac
 import javax.crypto.spec.SecretKeySpec
-import javax.servlet.FilterChain
-import javax.servlet.http.HttpServletRequest
-import javax.servlet.http.HttpServletResponse
 
 @Component
 class WebhookSignatureVerificationFilter : OncePerRequestFilter() {
