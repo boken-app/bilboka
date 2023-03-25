1298b5244c6c2072109a3b14add00e89406b9f06
IvarAlek <ivar.gj@hotmail.com>
2023-03-25 15:32:04 +0100

    Test å fjerne flyway repair.

--- bilboka-configuration/src/main/kotlin/bilboka/config/CarBookSpringConfig.kt
+++ bilboka-configuration/src/main/kotlin/bilboka/config/CarBookSpringConfig.kt
@@ -1,7 +1,5 @@
 package bilboka.config
 
-import org.flywaydb.core.Flyway
-import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.web.servlet.LocaleResolver
@@ -17,12 +15,12 @@ class CarBookSpringConfig {
         slr.setDefaultLocale(Locale.GERMANY)
         return slr
     }
-
-    @Bean
-    fun cleanMigrateStrategy(): FlywayMigrationStrategy? {
-        return FlywayMigrationStrategy { flyway: Flyway ->
-            flyway.repair()
-            flyway.migrate()
-        }
-    }
+//
+//    @Bean
+//    fun cleanMigrateStrategy(): FlywayMigrationStrategy? {
+//        return FlywayMigrationStrategy { flyway: Flyway ->
+//            flyway.repair()
+//            flyway.migrate()
+//        }
+//    }
 }
