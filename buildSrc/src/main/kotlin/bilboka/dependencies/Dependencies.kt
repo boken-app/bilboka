7324d70fd8f2fd81fa51c0b2ec56a264fc0a5f22
IvarAlek <ivar.gj@hotmail.com>
2023-04-04 02:00:57 +0200

    Oppgradere Spring og logging (roter til Heroku)

--- buildSrc/src/main/kotlin/bilboka/dependencies/Dependencies.kt
+++ buildSrc/src/main/kotlin/bilboka/dependencies/Dependencies.kt
@@ -1,14 +1,14 @@
 package bilboka.dependencies
 
 object Versions {
-    const val spring = "2.6.3"
+    const val springBoot = "3.0.4"
 
     const val kotlin = "1.8.10"
     const val jUnitJupiter = "5.9.2"
     const val jUnitPlatform = "1.9.2"
 
-    const val slf4j = "1.7.30"
-    const val logback = "1.2.3"
+    const val slf4j = "2.0.7"
+    const val logback = "1.3.6"
 
     const val exposed = "0.41.1"
     const val okHttp = "4.10.0"
@@ -17,8 +17,8 @@ object Versions {
 }
 
 object Libs {
-    const val springbootDependencies = "org.springframework.boot:spring-boot-dependencies:${Versions.spring}"
-    const val springbootGradle = "org.springframework.boot:spring-boot-gradle-plugin:${Versions.spring}"
+    const val springbootDependencies = "org.springframework.boot:spring-boot-dependencies:${Versions.springBoot}"
+    const val springbootGradle = "org.springframework.boot:spring-boot-gradle-plugin:${Versions.springBoot}"
     const val okHttp = "com.squareup.okhttp3:okhttp-bom:${Versions.okHttp}"
 
     const val ktorServerNetty = "io.ktor:ktor-server-netty:${Versions.kTor}"
