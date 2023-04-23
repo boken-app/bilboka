2285acffe9994293a2fa595c2279146c46c510b3
IvarAlek <ivar.gj@hotmail.com>
2023-04-23 19:59:23 +0200

    Trengs de her?

--- bilboka-autosys-integration/build.gradle.kts
+++ bilboka-autosys-integration/build.gradle.kts
@@ -11,14 +11,12 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
     implementation(platform(bilboka.dependencies.Libs.okHttp))
     implementation("com.squareup.okhttp3:okhttp")
     implementation("com.squareup.okhttp3:logging-interceptor")
 
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("com.squareup.okhttp3:mockwebserver")
 }
 
