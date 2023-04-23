405f86372d2bf086140392f47660c3c44ad02c87
IvarAlek <ivar.gj@hotmail.com>
2023-04-23 19:58:23 +0200

    Trenger ikke spring boot starter.

--- bilboka-autosys-integration/build.gradle.kts
+++ bilboka-autosys-integration/build.gradle.kts
@@ -11,7 +11,6 @@ version = "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_17
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 
