e21f2484cc3b0dd98f289f2942301422811e45f1
IvarAlek <ivar.gj@hotmail.com>
2024-01-21 18:25:04 +0100

    Fungerende dekryptering av email

--- bilboka-configuration/src/main/kotlin/bilboka/security/AuthenticationService.kt
+++ bilboka-configuration/src/main/kotlin/bilboka/security/AuthenticationService.kt
@@ -1,35 +1,68 @@
 package bilboka.security
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.security.authentication.BadCredentialsException
 import org.springframework.security.core.authority.AuthorityUtils
 import org.springframework.stereotype.Service
+import java.security.KeyFactory
+import java.security.PrivateKey
+import java.security.spec.PKCS8EncodedKeySpec
 import java.util.*
+import javax.crypto.Cipher
 import javax.servlet.http.HttpServletRequest
 
 @Service
 class AuthenticationService {
     companion object {
         const val AUTH_TOKEN_HEADER = "X-API-KEY"
+        private val log: Logger = LoggerFactory.getLogger(AuthenticationService::class.java)
     }
 
-    @Value("\${bilboka.web.secret}")
-    lateinit var apiSecret: String
+    @Value("\${bilboka.web.privateKey}")
+    lateinit var privateKey: String
 
     fun getAuthentication(request: HttpServletRequest): ApiKeyAuthentication {
         val token = request.getHeader(AUTH_TOKEN_HEADER)
-        val user = getUserByEmail(decodeTokenToEmail(token)) ?: throw BadCredentialsException("Invalid API key")
+        val user = token?.let { getUserByEmail(decodeTokenToEmail(it)) }
+            ?: throw BadCredentialsException("Invalid API key")
         return ApiKeyAuthentication(user, AuthorityUtils.NO_AUTHORITIES)
     }
 
-    private fun decodeTokenToEmail(token: String?): String? {
-        return token?.let { // TODO implementere dekoding av token med secret
-            try {
-                String(Base64.getDecoder().decode(it))
-            } catch (e: IllegalArgumentException) {
-                return null
+    private fun decodeTokenToEmail(token: String): String? {
+        val cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding")
+            .apply { init(Cipher.DECRYPT_MODE, privateKey()) }
+
+        val decodedBytes = Base64.getDecoder().decode(token)
+        val decryptedBytes = cipher.doFinal(decodedBytes)
+
+        return String(decryptedBytes)
+            .also { log.info("Dekryptert token: {}", it) }
+            .getAsEmail()
+            .also {
+                if (it != null) log.info("Dekryptert token til epost {}", it)
+                else log.info("Dekryptert token er ikke en gyldig epost")
             }
-        }
+    }
+
+    fun privateKey(): PrivateKey {
+        val privateKeyPEM = privateKey
+            .replace("-----BEGIN PRIVATE KEY-----", "")
+            .replace("-----END PRIVATE KEY-----", "")
+            .replace("\\s".toRegex(), "")
+
+        val encoded = Base64.getDecoder().decode(privateKeyPEM)
+        val keySpec = PKCS8EncodedKeySpec(encoded)
+        val keyFactory = KeyFactory.getInstance("RSA")
+
+        return keyFactory.generatePrivate(keySpec)
+    }
+
+    fun String.getAsEmail(): String? {
+        return if (this.isNotEmpty())
+            Regex("^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}\$").matchEntire(this)?.value
+        else null
     }
 
     private fun getUserByEmail(email: String?): String? {
--- bilboka-configuration/src/test/kotlin/bilboka/web/SecurityIntegrationTest.kt
+++ bilboka-configuration/src/test/kotlin/bilboka/web/SecurityIntegrationTest.kt
@@ -14,7 +14,11 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers
+import java.security.KeyFactory
+import java.security.PublicKey
+import java.security.spec.X509EncodedKeySpec
 import java.util.*
+import javax.crypto.Cipher
 
 @ExtendWith(SpringExtension::class)
 @WebMvcTest(VehicleResource::class)
@@ -59,6 +63,22 @@ class SecurityIntegrationTest {
     }
 
     @Test
+    fun `test get vehicles authorized then unauthorized`() {
+        mockMvc.perform(
+            get("/vehicles")
+                .header("X-API-KEY", encodeEmail("some.test@mail.com"))
+                .accept(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(MockMvcResultMatchers.status().isOk)
+        mockMvc.perform(
+            get("/vehicles")
+                .header("X-API-KEY", encodeEmail("some.new.test@mail.com"))
+                .accept(MediaType.APPLICATION_JSON)
+        )
+            .andExpect(MockMvcResultMatchers.status().isUnauthorized)
+    }
+
+    @Test
     fun `test get vehicle user not existing`() {
         mockMvc.perform(
             get("/vehicles")
@@ -69,8 +89,27 @@ class SecurityIntegrationTest {
     }
 
     private fun encodeEmail(email: String): String {
-        return String(Base64.getEncoder().encode(email.toByteArray()))
+        val cipher = Cipher.getInstance("RSA/ECB/OAEPWithSHA-256AndMGF1Padding")
+            .also { it.init(Cipher.ENCRYPT_MODE, publicTestKey.toPublicKey()) }
+
+        val encryptedBytes = cipher.doFinal(email.toByteArray())
+        return Base64.getEncoder().encodeToString(encryptedBytes)
     }
+
+    private fun String.toPublicKey(): PublicKey {
+        val keyBytes = Base64.getDecoder().decode(this)
+        val keySpec = X509EncodedKeySpec(keyBytes)
+        val keyFactory = KeyFactory.getInstance("RSA")
+        return keyFactory.generatePublic(keySpec)
+    }
+
+    private val publicTestKey = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAyL/HemQivAm/LRAu8V0y" +
+            "xXqGS8NugpFkqsK+GNJv/iksspDpQV7SQuuZDn/YJ+OIUS80q15shScNFQoz207G" +
+            "wasD73BRPo8th7DBLjh/7B+tVx03JGkRwEuICjclmAIkTQTBIPyDDqJ8o5cvrpqL" +
+            "zSdoNvX/ylRlH7EGpIMaSMlynfrFyGkRJSJzY+iZIyB8qhxrPa6WiTYNFqgd49LH" +
+            "uKtT1IXsUMPx+VTKXCRaJ6LNHNoPaZOk/JPpg5Ie2QbSe7E5y9KdwQ1ScNSZG584" +
+            "NMvYElePUWQ7xLTu40P13jbVuJQiDm8Vg74wNUvnPezby2X4CMTFnswMLSrWLv7E" +
+            "1QIDAQAB"
 
     @Configuration
     @ComponentScan(basePackages = ["bilboka"])
