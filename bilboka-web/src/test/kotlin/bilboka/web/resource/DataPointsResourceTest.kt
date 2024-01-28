144790109412266aa348bb54c73bcaf9d808faca
IvarAlek <ivar.gj@hotmail.com>
2024-01-28 10:50:06 +0100

    Datapoints med tom liste

--- bilboka-web/src/test/kotlin/bilboka/web/resource/DataPointsResourceTest.kt
+++ bilboka-web/src/test/kotlin/bilboka/web/resource/DataPointsResourceTest.kt
@@ -1,20 +1,28 @@
 package bilboka.web.resource
 
+import bilboka.core.vehicle.VehicleService
+import io.mockk.impl.annotations.MockK
+import io.mockk.junit5.MockKExtension
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.test.web.servlet.MockMvc
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 import org.springframework.test.web.servlet.setup.MockMvcBuilders
 
+@ExtendWith(MockKExtension::class)
 class DataPointsResourceTest {
 
     private lateinit var mockMvc: MockMvc
 
+    @MockK
+    private lateinit var vehicleService: VehicleService
+
     @BeforeEach
     fun setup() {
         mockMvc = MockMvcBuilders
-            .standaloneSetup(DataPointsResource())
+            .standaloneSetup(DataPointsResource(vehicleService))
             .build()
     }
 
