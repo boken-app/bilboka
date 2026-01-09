004aae6bba25d03e62a668d2617b499a459a3a9e
IvarAlek <ivar.gj@outlook.com>
2026-01-09 01:41:32 +0100

    Tror ikke vi trenger mockito her

--- bilboka-messenger-integration/build.gradle.kts
+++ bilboka-messenger-integration/build.gradle.kts
@@ -20,7 +20,6 @@ dependencies {
     implementation("com.squareup.okhttp3:okhttp")
     implementation("com.squareup.okhttp3:logging-interceptor")
 
-    testImplementation("org.mockito:mockito-core:3.3.3") // TODO trenger vi mockito?
     testImplementation("io.mockk:mockk:1.10.6") // Feilet ved nyere versjon
     testImplementation("com.ninja-squad:springmockk:3.0.1")
     testImplementation("com.squareup.okhttp3:mockwebserver")
